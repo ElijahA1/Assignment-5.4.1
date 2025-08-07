60fb24f4ea52373b2e39ea198c675953e12222ea
ElijahA1 <elijah.abourezk@outlook.com>
2025-08-07 14:27:35 -0700

    Add project files.

---
+++ Assignment 5.4.1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Assignment_5._4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int test = 1234;
+
+            displayDigitsRecursively(test);
+        }
+        static void displayDigitsRecursively(int number)
+        {
+            if (number == 0) 
+            {
+                return;
+            }
+
+            displayDigitsRecursively(number / 10);
+
+            Console.WriteLine(number % 10);
+        }
+    }
+}
